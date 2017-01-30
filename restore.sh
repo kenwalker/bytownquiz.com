e0cdce73ca0a2cd7f45ea4f8671cbab2449b3fe0
Ken Walker <ken_walker@ca.ibm.com>
2017-01-29 20:51:03 -0500

    more work on info

---
+++ restore.sh
@@ -0,0 +1,6 @@
+git checkout favicon.png
+git checkout img/group_shot1.jpg
+git checkout img/group_shot2.jpg
+git checkout img/placeholder-image.png
+git checkout img/record.png
+git checkout img/vinyl_record.png
